8b6b20423f220c6864a23f7102a23a745ae99e7f
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-06-06 09:59:57 -0600

    iteration one with passing tests

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -3,11 +3,33 @@
 
 class Pantry
 
-  attr_reader :stock 
+  attr_accessor :stock
 
   def initialize
     @stock = {}
 
   end
 
+  def stock_check(item)
+    @stock[item] || 0
+  end
+
+  def restock(ingredient, quantity)
+    if @stock[ingredient]
+      @stock[ingredient] += quantity
+    else
+      @stock = {ingredient => quantity}
+    end
+  end 
+
 end
+
+
+# counted_animals = {}
+# animals.each do |animal|
+#   if counted_animals[animal]
+#     counted_animals[animal] += 1
+#   else
+#     counted_animals[animal] = 1
+#   end
+# end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -11,5 +11,36 @@ class PantryTest < Minitest::Test
     assert_instance_of Pantry, pantry
   end
 
-  
+  def test_it_pantry_has_stock_as_hash
+    pantry = Pantry.new
+
+    assert_equal Hash, pantry.stock.class
+  end
+
+  def test_for_check_items_stock
+    pantry = Pantry.new
+    result = pantry.stock_check("Cheese")
+
+    assert_equal 0, result
+  end
+
+  def test_you_can_restock_pantry
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    result = pantry.stock_check("Cheese")
+
+    assert_equal 10, result
+  end
+
+  def test_pantry_stock_accumulates
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    pantry.restock("Cheese", 20)
+
+    result = pantry.stock_check("Cheese")
+
+    assert_equal 30, result
+  end
+
+
 end
