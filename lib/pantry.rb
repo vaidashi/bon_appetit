21c4331d54950b53b010e31e23eda53dcd2b079c
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-06-06 09:14:28 -0600

    first test created and passing for pantry

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,13 @@
+
+
+
 class Pantry
+
+  attr_reader :stock 
+
+  def initialize
+    @stock = {}
+
+  end
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,15 @@
 require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'minitest/emoji'
 
 class PantryTest < Minitest::Test
 
+  def test_class_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  
 end
