60cf5be6a610cae56fb0178c7d5514a626b856ff
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-06-06 11:53:35 -0600

    couldn't get numbers to convert

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -23,20 +23,33 @@ class Pantry
     end
   end
 
+  # def convert_units(recipe)
+  #   @recipe.ingredients.map do |key, value|
+  #     if value > 100
+  #        value / 100
+  #     elsif value < 1
+  #        value * 1000
+  #     else
+  #        value * 1
+  #     end
+  #   end
+  # end
+
   def convert_units(recipe)
-    @recipe.ingredients.map do |key, value|
-      if value > 100
-         value / 100
-      elsif value < 1
-         value * 1000
-      else
-         value * 1
-      end
-    end
+    recipe = @recipe.ingredients
+    recipe.each_value { |value| converter(value) }
+  # binding.pry
   end
 
-
-
+  def converter(num)
+    if num > 100
+      num / 100
+    elsif num < 1
+      num * 1000
+    else
+      num
+    end
+  end
 
 # binding.pry
 end
